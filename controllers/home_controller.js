48513159187328750697ddba11a41b3127d50c83
Harshit Pandey <harshitpandey@Harshits-MacBook-Air.local>
2021-10-13 14:41:39 +0530

    Completing adding data

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,17 @@
 // Home Controller
+const task = require('../models/task');
 module.exports.home = function(req,res){
-    return res.render('home.ejs',{
-
-        title : "ToDo App"
-
+    
+    task.find({},function(err ,task){
+        if(err)
+        console.log('There was an error in the DB');
+        console.log(task);
+        return res.render('home.ejs',{
+            title: "ToDo Application",
+            taskList: task
+        });
     });
 }
+
+
+
